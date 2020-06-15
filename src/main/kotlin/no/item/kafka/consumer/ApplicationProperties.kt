cf2426f639bfb351dff9206e0dde3cb63d950996
mortenjuult <morten.torke@item.no>
2020-06-15 14:33:56 +0200

    Changes to IDE-config

--- src/main/kotlin/no/item/kafka/consumer/ApplicationProperties.kt
+++ src/main/kotlin/no/item/kafka/consumer/ApplicationProperties.kt
@@ -1,15 +1,15 @@
 package no.item.kafka.consumer
 
-import org.apache.kafka.common.serialization.StringDeserializer
 import java.util.*
+import org.apache.kafka.common.serialization.StringDeserializer
 
-//TODO: gå gjennom configen og finne ut hva som trengs for å knyttes mot entur...
-//https://kafka.apache.org/documentation/#consumerconfigs
+// TODO: gå gjennom configen og finne ut hva som trengs for å knyttes mot entur...
+// https://kafka.apache.org/documentation/#consumerconfigs
 fun getProperties(): Properties {
-    val properties = Properties()
-    properties["bootstrap.servers"] = ""
-    properties["key.deserializer"] = StringDeserializer::class.java
-    properties["value.deserializer"] = StringDeserializer::class.java
-    properties["group.id"] = "group-id"
-    return properties
+  val properties = Properties()
+  properties["bootstrap.servers"] = ""
+  properties["key.deserializer"] = StringDeserializer::class.java
+  properties["value.deserializer"] = StringDeserializer::class.java
+  properties["group.id"] = "group-id"
+  return properties
 }
--- src/main/kotlin/no/item/kafka/consumer/MockKafkaConsumer.kt
+++ src/main/kotlin/no/item/kafka/consumer/MockKafkaConsumer.kt
@@ -1,32 +1,31 @@
 package no.item.kafka.consumer
 
+import java.time.Duration
+import java.util.*
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.time.Duration
-import java.util.*
 
-
-fun main(args:Array<String>){
-    MockKafkaConsumer().run()
+fun main(args: Array<String>) {
+  MockKafkaConsumer().run()
 }
 
 class MockKafkaConsumer {
-    private val logger: Logger = LoggerFactory.getLogger(javaClass)
-    fun run(){
-        logger.info("Starting consumer")
-        val props: Properties = getProperties()
-        val consumer: KafkaConsumer<String, String> = KafkaConsumer<String, String>(props)
-        consumer.subscribe(Collections.singleton("entur.vehicle.activity"))
+  private val logger: Logger = LoggerFactory.getLogger(javaClass)
+  fun run() {
+    logger.info("Starting consumer")
+    val props: Properties = getProperties()
+    val consumer: KafkaConsumer<String, String> = KafkaConsumer<String, String>(props)
+    consumer.subscribe(Collections.singleton("entur.vehicle.activity"))
 
-        while(true) {
-            val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofMillis(100))
-            logger.info("Got some records...")
-            records.iterator().forEach {
-                val record = it.value()
-                //TODO:do something with this record...
-            }
-        }
+    while (true) {
+      val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofMillis(100))
+      logger.info("Got some records...")
+      records.iterator().forEach {
+        val record = it.value()
+        // TODO:do something with this record...
+      }
     }
+  }
 }
